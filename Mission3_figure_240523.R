3ebcf9a0179bf8cbcf64e680456d4e89375892eb
ywkim28 <ywkim28@snu.ac.kr>
2024-05-23 22:27:43 +0900

    homework

---
+++ Mission3_figure_240523.R
@@ -0,0 +1,35 @@
+## setting directory
+setwd("/Users/ywkim/Desktop/bioinfo1_data/")
+
+## load library
+library(DescTools)
+library(stringr)
+library(dplyr)
+
+## data import
+data <- read.table("CLIP-let7g-gene.pileup")
+colnames(data) <- c("chrom", "pos", "_ref", "count", "basereads", "quals")
+
+## filtering match and substitution
+matches <- gsub("[<>$*#^]", "", data$basereads)
+data <- cbind(data, matches)
+
+## filtering data
+data <- data %>% filter(matches != "")
+
+## calculate shannon entropy
+entropy <- c()
+
+for (i in data$matches){
+  tmp <- substring(i, 1:nchar(i), 1:nchar(i))
+  
+  entropy <- c(entropy, Entropy(table(tmp)/nchar(i)))
+}
+
+## make bedGraph dataframe
+bedGraph <- data.frame(chrom      = data$chrom,
+                       chromStart = data$pos,
+                       chromEnd   = data$pos+1,
+                       dataValue  = entropy)
+
+write.table(bedGraph, "bedGraph.tsv", sep = "\t")
